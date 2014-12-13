de37153689100adb50848f978d906f19a8493a81
monkeyde17 <monkey_tv@126.com>
2014-12-13 22:33:45 +0800

    init the proj

---
+++ proj/ETFMoive/ETFMoive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ETFMoive
+//
+//  Created by etond on 14/12/13.
+//  Copyright (c) 2014年 etond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ proj/ETFMoive/ETFMoive/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ETFMoive
+//
+//  Created by etond on 14/12/13.
+//  Copyright (c) 2014年 etond. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
